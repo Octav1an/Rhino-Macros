7eeb44d9bc34a272f50dadaa5839cdc58d79acd2
octav1an <octav1an@hotmail.com>
2015-12-12 09:14:14 +0100

    New macro, changes the current layer to the layer of selected object

---
+++ ActivateLayerSelelectedObj.py
@@ -0,0 +1,20 @@
+import rhinoscriptsyntax as rs
+
+
+def activateLayerSelectedObj():
+    ###Make the current layer the selected object layer
+    ###Select object
+    obj_ref = rs.GetObject("Select Object", 0, True)
+    ###Get the object layer
+    obj_layer = rs.ObjectLayer(obj_ref)
+    ###Make current layer the object layer
+    if obj_layer != None:
+        rs.CurrentLayer(obj_layer)
+    else:
+        return
+        ###Print the name of the current layer
+    print("Layer {} activated.".format(obj_layer))
+    
+
+
+activateLayerSelectedObj()
