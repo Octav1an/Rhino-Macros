ba6bb71f3b990cbcff61b9143c3f79a3c26d89fb
octav1an <octav1an@hotmail.com>
2015-12-14 05:25:59 +0100

    Adding new icons for IterateCopy and SendToInitPos Macros, Fixing the bug in IterateCopy, which didn't allow multiple iteration branches, and fixing the translation distance displayed in parent ITERATE layer

--- IterateCopy.py
+++ IterateCopy.py
@@ -14,15 +14,17 @@ def iterateCopy():
     final_dist = 0
     if boxCorners:
         ###Cheking the distance x-y
+        ###final_dist is the translation dist for iteration
         dist1 = rs.Distance(boxCorners[0], boxCorners[1])
         dist2 = rs.Distance(boxCorners[1], boxCorners[2])
         if dist1 > dist2:
-            final_dist = dist1
+            final_dist = dist1 * 2
         else:
-            final_dist = dist2
+            final_dist = dist2 * 2
     else: return
     
     allLayers = rs.LayerNames()
+    print(allLayers[1])
     ###Boolean execute once and if False
     bll_once = False
     ###Making sure we dont repeat ITERATE parent layer 
@@ -30,7 +32,7 @@ def iterateCopy():
         if layer.find("ITERATE {}".format(nrObjIteration)) >= 0 and not bll_once:
             bll_once = True
             strParentLayer = layer
-
+    ###Create a new ITERATE layer if it doesnt exists
     if not bll_once:
         strParentLayer = rs.AddLayer("ITERATE {} [{:1.1f}] <{:1.1f}, {:1.1f}, {:1.1f}>"\
         .format(nrObjIteration, final_dist, in_ptbase[0], in_ptbase[1], in_ptbase[2]))
@@ -41,8 +43,8 @@ def iterateCopy():
     nrIterateChild = rs.LayerChildCount(strParentLayer) + 1
     ###Create sublayer for each iteration
     ###Easier to do the translation math later on
-    strLayer = mf.layerCheck("I - ({})".format(nrIterateChild),strParentLayer)
-    
+    strLayer = rs.AddLayer("I - ({})".format(nrIterateChild), color = \
+    (110, 200, 70), parent = strParentLayer)
     ###Go through all the selected objects and check their layers,
     ###If a least one is form a Iterate child layer do the math for translation...
     I_count = 0
@@ -50,12 +52,12 @@ def iterateCopy():
         obj_l = rs.ObjectLayer(obj)
         if obj_l.find("I - (") >= 0:
             I_count = obj_l[obj_l.find("(")+1:obj_l.find(")")]
-            #print(I_count)
         else: I_count = 0
+    ###Copy the objects with the necessary distance which is the double of the
+    ###longest edge of the bounding box of First selected objects
     obj_iteration = rs.CopyObjects(obj_ref, ((float(distParent) * \
-    (int(nrIterateChild) - int(I_count)))*2, 0, 0))
+    (int(nrIterateChild) - int(I_count))), 0, 0))
     rs.ObjectLayer(obj_iteration, strLayer)
-    #print(obj_iteration)
 
 
 
